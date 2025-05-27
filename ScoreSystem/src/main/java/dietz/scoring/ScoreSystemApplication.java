cae77b2b3fc57c96235242e878556be233fdded5
dietzdenstore <jonasmogelgaard@gmail.com>
2025-05-27 19:01:06 +0200

    MicroServices Implemented - Score Increments when destoying asteroids and enemies.

--- ScoreSystem/src/main/java/dietz/scoring/ScoreSystemApplication.java
+++ ScoreSystem/src/main/java/dietz/scoring/ScoreSystemApplication.java
@@ -7,29 +7,33 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @RestController
-@CrossOrigin   // allow your JavaFX app (localhost) to call it
+@CrossOrigin  // allow your JavaFX app to call it
 public class ScoreSystemApplication {
 
+    // in-memory score
     private int score = 0;
 
     public static void main(String[] args) {
-        SpringApplication.run(dietz.scoring.ScoreSystemApplication.class, args);
+        SpringApplication.run(ScoreSystemApplication.class, args);
     }
 
+    /** GET current score */
     @GetMapping("/score/get")
     public int getScore() {
         return this.score;
     }
 
-    @PutMapping("/score/add/{delta}")
-    public int addToScore(@PathVariable int delta) {
-        this.score += delta;
-        return this.score;
-    }
-
+    /** RESET score to a specific value */
     @PutMapping("/score/set/{value}")
     public int setScore(@PathVariable("value") int value) {
         this.score = value;
+        return this.score;
+    }
+
+    /** ADD delta to the score */
+    @PutMapping("/score/add/{delta}")
+    public int addToScore(@PathVariable("delta") int delta) {
+        this.score += delta;
         return this.score;
     }
 }
